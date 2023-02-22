6dd8b51f5b397b6abdea0b15c1e7717b6ac8c9b8
Mahdi-Fada <mahdifada.eddini@outlook.com>
2023-02-22 11:09:35 +0330

    improve shop analytic abstraction and add new extension methods

--- Runtime/Abstraction/IShopAnalytic.cs
+++ Runtime/Abstraction/IShopAnalytic.cs
@@ -2,9 +2,13 @@
 {
     public interface IShopAnalytic
     {
-        void PurchaseShopPack(string packId);
-        void ShopPackClick(string itemName, string location);
-        void ShopOpen(string location);
-        void PurchaseShopPack(string itemName, string purchaseId, string currencyId, float price, string location, string transactionId);
+
+        void ShopOpen(string placement);
+        void PurchaseStartSuccess(string sku, string packName, string currency, double price, string orderId);
+        void PurchaseStartFailed(string sku, string packName, string reason);
+        void PurchaseSuccessResult(string sku, string packName, string currency, double price, string orderId);
+        void PurchaseFailedResult(string sku, string packName, string currency, double price, string orderId, string reason);
+        void PurchaseComplete(string sku, string packName, string currency, double price, string orderId);
+        void ShopPackClick(string sku, string packName);
     }
 }
